e0ecb6f46fd0c24f2ccd469de41871a54c762107
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-08 14:16:23 -0300

    Criando os projetos Spring e React.

---
+++ backend/src/test/java/com/rafadev/rdvendas/RdvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafadev.rdvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
