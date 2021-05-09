9fd16a00ab0a44c8764279a73dfcdea8c6c5d62f
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-08 21:07:11 -0300

    Adicionando componentes básicos NavBar, Footer e DataTable.

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light mb-0">
+                    <small>App desenvolvido por <a href="https://br.linkedin.com/in/rafael-carreiro-campos-9aa4441b4" target="_blank" rel="noreferrer">Rafael Campos </a>
+                    durante a <strong>Semana Spring React</strong></small>
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
