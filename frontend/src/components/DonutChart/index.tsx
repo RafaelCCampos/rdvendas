499c4c4aabac79fd43d9df447cbbab4bc653ff91
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-10 02:21:23 -0300

    add types, axios and first request.

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,23 @@
 import Chart from 'react-apexcharts';
+import api from 'services/api';
+import { SaleSum } from 'types/sale';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
+
+    api.get('/sales/sum-by-seller')
+        .then(response => {
+            const data: SaleSum[] = response.data
+            const myLabels = data.map(x => x.sellerName)
+            const mySeries = data.map(x => x.sum)
+
+            const chartData = {labels: myLabels, series: mySeries}
+            console.log(chartData);
+        })
 
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
