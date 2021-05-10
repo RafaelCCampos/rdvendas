118cffa999b869f0754ec147baf6f5430cb323af
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-09 22:03:09 -0300

    Repository, Service, Controller, DTO para os Sellers

---
+++ backend/src/main/java/com/rafadev/rdvendas/controller/SellerController.java
@@ -0,0 +1,26 @@
+package com.rafadev.rdvendas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafadev.rdvendas.dto.SellerDTO;
+import com.rafadev.rdvendas.service.SellerService;
+
+@RestController
+@RequestMapping( value = "/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll() {
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
---
+++ backend/src/main/java/com/rafadev/rdvendas/repository/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.rafadev.rdvendas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafadev.rdvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
