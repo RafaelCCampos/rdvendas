8a9c4584b955f4374bcc29c59ae347e3f5453ae3
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-12 01:39:51 -0300

    Donut Chart dinamic

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import api from 'services/api';
 import { SaleSum } from 'types/sale';
@@ -8,22 +9,22 @@ type ChartData = {
 }
 
 const DonutChart = () => {
+    const [chartData, setChartData ] = useState({
+        labels: [''],
+        series: [0],
+    });
 
-    api.get('/sales/sum-by-seller')
+    useEffect(() => {
+        api.get('/sales/sum-by-seller')
         .then(response => {
             const data: SaleSum[] = response.data
             const myLabels = data.map(x => x.sellerName)
             const mySeries = data.map(x => x.sum)
 
-            const chartData = {labels: myLabels, series: mySeries}
-            console.log(chartData);
+            setChartData({labels: myLabels, series: mySeries})
         })
+    },[])
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
     const options = {
         legend: {
             show: true
@@ -32,8 +33,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
