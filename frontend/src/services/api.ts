662eb1d634f7bba9b31ce16b1b563d40a22e7943
Rafael Campos <rafaelccamposdev@gmail.com>
2021-05-16 17:23:19 -0300

    env var

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const BASE_URL = process.env.BACKEND_URL ?? 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const api = Axios.create({
     baseURL: BASE_URL,
